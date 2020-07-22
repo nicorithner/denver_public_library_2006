75f224c0e5010762057814d5379746e528228cd3
Nico Rithner <nicorithner@gmail.com>
2020-07-22 13:09:19 -0600

    Add method it has a name for author class:

--- lib/author.rb
+++ lib/author.rb
@@ -1,3 +1,9 @@
 class Author
+  attr_reader :name
+  def initialize(data)
+  @first_name = data[:first_name]
+  @last_name = data[:last_name]
+  @name = "#{@first_name} #{@last_name}"
+  end
 
 end
--- test/author_test.rb
+++ test/author_test.rb
@@ -7,7 +7,14 @@ require "./lib/library"
 require "pry"
 
 class AuthorTest < MiniTest::Test
+  
   def test_it_exists
-    charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"}
-  assert_instance_of Author, charlotte_bronte
+    charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+    assert_instance_of Author, charlotte_bronte
   end
+
+  def test_it_has_a_name
+    charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+    assert_equal "Charlotte Bronte", charlotte_bronte.name
+  end
+end
