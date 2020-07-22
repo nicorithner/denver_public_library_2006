3a468593db911764f87387057361bf205bee64d7
Nico Rithner <nicorithner@gmail.com>
2020-07-22 13:03:26 -0600

    Add test it exist method for Author class

--- test/author_test.rb
+++ test/author_test.rb
@@ -5,3 +5,9 @@ require "./lib/author"
 require "./lib/library"
 
 require "pry"
+
+class AuthorTest < MiniTest::Test
+  def test_it_exists
+    charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"}
+  assert_instance_of Author, charlotte_bronte
+  end
