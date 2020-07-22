80f720159f5c73afd6e081220275bc30cdc440e3
Nico Rithner <nicorithner@gmail.com>
2020-07-22 14:08:19 -0600

    Add test it exists and test it has a name tests. Create class Library

--- test/library_test.rb
+++ test/library_test.rb
@@ -5,3 +5,16 @@ require "./lib/author"
 require "./lib/library"
 
 require "pry"
+
+class LibraryTest < MiniTest::Test
+  
+  def test_it_exists
+    dpl = Library.new("Denver Public Library")
+    assert_instance_of Library, dpl
+  end
+
+  def test_it_has_a_name
+    dpl = Library.new("Denver Public Library")
+    assert_equal "Denver Public Library", dpl.name
+  end
+end
