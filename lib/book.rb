9f979d3337967fe2a4e85c0638aaeb35f31c7792
Nico Rithner <nicorithner@gmail.com>
2020-07-22 13:01:13 -0600

    Add it has a publication year method

--- lib/book.rb
+++ lib/book.rb
@@ -1,10 +1,12 @@
 class Book
-  attr_reader :title, :author_first_name, :author_last_name, :author
+  attr_reader :title, :author_first_name, :author_last_name, :author, :publication_year
   def initialize(data)
     @title = data[:title]
     @author_first_name = data[:author_first_name]
     @author_last_name = data[:author_last_name]
     @author = "#{@author_first_name} #{@author_last_name}"
+    @publication_date = data[:publication_date]
+    @publication_year = @publication_date.slice(9..12)
   end
 
 
--- test/book_test.rb
+++ test/book_test.rb
@@ -21,4 +21,9 @@ class BookTest < MiniTest::Test
     book = Book.new({author_first_name: "Harper", author_last_name: "Lee", title: "To Kill a Mockingbird", publication_date: "July 11, 1960"})
   assert_equal "Harper Lee", book.author
   end
+
+  def test_it_has_an_publication_year
+    book = Book.new({author_first_name: "Harper", author_last_name: "Lee", title: "To Kill a Mockingbird", publication_date: "July 11, 1960"})
+  assert_equal "1960", book.publication_year
+  end
 end
